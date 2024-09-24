baff8f0758d5f331bbd06678d274858dabc824df
Iberlauf <iberlauf@gmail.com>
2024-09-24 09:13:08 +0200

    Updated the models module.

--- models.py
+++ models.py
@@ -1,9 +1,24 @@
 """Models module."""
 
-from sqlmodel import SQLModel, Field
+from enum import Enum
+from sqlmodel import SQLModel, Field, Relationship
+from pydantic import EmailStr
 
 
-class AdressBase(SQLModel):
+class BusinessType(str, Enum):
+    """Type enum."""
+
+    ELECTRICAL = "electrical"
+    WATER = "water"
+    CABLE = "cable"
+    CLEANING = "cleaning"
+    PHONE = "phone"
+    TAX = "tax"
+    HEATING = "heating"
+    GARBAGE = "garbage"
+
+
+class AddressBase(SQLModel):
     """Adress base model."""
 
     street: str
@@ -12,7 +27,43 @@ class AdressBase(SQLModel):
     postal_code: int
 
 
-class AdressCreate(AdressBase, table=True):
+class AddressCreate(AddressBase, table=True):
     """Adress create model."""
 
-    id: int = Field(default=None, primary_key=True)
+    id: int | None = Field(default=None, primary_key=True)
+    users: list["UserBase"] = Relationship(back_populates="address")
+    businesses: list["BusinessBase"] = Relationship(back_populates="address")
+
+
+class UserBase(SQLModel):
+    """User base model."""
+
+    first_name: str
+    last_name: str
+    email: EmailStr
+
+
+class UserCreate(UserBase, table=True):
+    """User create model."""
+
+    id: int | None = Field(default=None, primary_key=True)
+    address_id: int = Field(default=None, foreign_key="addresscreate.id")
+    address: AddressCreate = Relationship(back_populates="users")
+
+
+class BusinessBase(SQLModel):
+    """Business base model."""
+
+    name: str
+    contact_email: EmailStr
+    bank_account: int
+    producer: str
+    type: BusinessType
+
+
+class BusinessCreate(BusinessBase, table=True):
+    """Business create model."""
+
+    id: int | None = Field(default=None, primary_key=True)
+    address_id: int = Field(default=None, foreign_key="addresscreate.id")
+    address: AddressCreate = Relationship(back_populates="businesses")
