50ccef40969079f5517d1034c1e49fec642462c7
Iberlauf <iberlauf@gmail.com>
2024-10-02 11:10:05 +0200

    Moved create_bill_from_pdf function to create_instances.py

--- create_instances.py
+++ create_instances.py
@@ -1,11 +1,17 @@
 """Module for creating SQLModel instances."""
 
-from datetime import date
+from datetime import date, datetime
 from decimal import Decimal
+from pathlib import Path
+
 from pydantic import EmailStr, HttpUrl
 from sqlmodel import Session
+
 from database import engine
+from main import select_business_by_account
 from models import Address, Bill, Business, BusinessType, User
+from pdf_reader import eps_pdf_reader, sbb_pdf_reader, vodovod_pdf_reader
+from qr_reader import clean_decimal, extract_dates, qr_reader
 
 
 def create_address(strt: str, nmb: str, cty: str, pst: int):
@@ -95,3 +101,52 @@ def create_bill(  # pylint: disable=(R0913:too-many-arguments)
         session.add(instance=new_bill)
         session.commit()
         session.refresh(instance=new_bill)
+
+
+def new_bill_from_pdf(pdf_path: Path) -> None:
+    """Creates a new Bill class instance/database entry from .pdf file."""
+    results: list[dict[str, str]] | None = qr_reader(pdf_path=pdf_path)
+    if results is not None:
+        result_dict: dict = results[0]
+        new_ammount: Decimal = clean_decimal(ammount_str=result_dict["I"])
+        new_business: Business | None = select_business_by_account(
+            account=int(result_dict["R"])
+        )
+        new_pay_code: str = results[0]["SF"]
+        if result_dict["S"]:
+            new_period: tuple[date, date] = extract_dates(date_string=result_dict["S"])
+        elif new_business and new_business.name == "SBB":
+            new_period = sbb_pdf_reader(pdf_path=pdf_path)
+        elif new_business and new_business.name == "EPS":
+            new_period = eps_pdf_reader(pdf_path=pdf_path)
+        if results[0]["RO"].startswith("97"):
+            new_model: str = results[0]["RO"][:2]
+            new_call_no: str = results[0]["RO"][2:]
+        else:
+            new_model: str = ""
+            new_call_no: str = results[0]["RO"]
+    else:
+        new_business: Business | None = select_business_by_account(
+            account=int(vodovod_pdf_reader(pdf_path=pdf_path)[0])  # type: ignore
+        )
+        new_ammount: Decimal = vodovod_pdf_reader(pdf_path=pdf_path)[1]  # type: ignore
+        new_model: str = ""
+        new_call_no: str = vodovod_pdf_reader(pdf_path=pdf_path)[2]  # type: ignore
+        new_period: tuple[date, date] = vodovod_pdf_reader(pdf_path=pdf_path)[3]  # type: ignore
+
+    if new_business and new_business.id is not None:
+        with Session(bind=engine) as session:
+            new_bill = Bill(
+                name=f"{new_business.name} from {new_period[0]} to {new_period[1]}",
+                payed=True,
+                date_payed=datetime.today(),
+                ammount=new_ammount,
+                pay_code=new_pay_code,
+                pay_model=new_model,
+                call_no=new_call_no,
+                business_id=new_business.id,
+                period=new_period,
+            )
+            session.add(instance=new_bill)
+            session.commit()
+            session.refresh(instance=new_bill)
--- qr_reader.py
+++ qr_reader.py
@@ -13,12 +13,6 @@ from pdf2image import convert_from_bytes
 from PIL.Image import Image
 from pyzbar import pyzbar
 
-from main import select_business_by_account
-
-# from create_instances import create_bill
-from models import Business
-from pdf_reader import sbb_pdf_reader, eps_pdf_reader, vodovod_pdf_reader
-
 
 def clean_decimal(ammount_str: str) -> Decimal:
     """Function for cleaning the ammount string.
@@ -92,48 +86,6 @@ def qr_reader(pdf_path: Path) -> list[dict[str, str]] | None:
     if result_list:
         return result_list
     return None
-
-
-pdfpath: Path = Path(r"")
-
-results: list[dict[str, str]] | None = qr_reader(pdf_path=pdfpath)
-if results is not None:
-    result_dict: dict = results[0]
-    new_amount: Decimal = clean_decimal(ammount_str=result_dict["I"])
-    new_business: Business | None = select_business_by_account(
-        account=int(result_dict["R"])
-    )
-    pay_code: str = results[0]["SF"]
-    if result_dict["S"]:
-        new_period: tuple[date, date] = extract_dates(date_string=result_dict["S"])
-    elif new_business and new_business.name == "SBB":
-        new_period = sbb_pdf_reader(pdf_path=pdfpath)
-    elif new_business and new_business.name == "EPS":
-        new_period = eps_pdf_reader(pdf_path=pdfpath)
-    if results[0]["RO"].startswith("97"):
-        new_model: str = results[0]["RO"][:2]
-        new_call_no: str = results[0]["RO"][2:]
-    else:
-        new_model: str = ""
-        new_call_no: str = results[0]["RO"]
-    print(new_amount, new_business, new_period, pay_code, new_model, new_call_no)
-else:
-    new_business: Business | None = select_business_by_account(
-        account=int(vodovod_pdf_reader(pdf_path=pdfpath)[0])  # type: ignore
-    )
-    new_ammount: Decimal = vodovod_pdf_reader(pdf_path=pdfpath)[1]  # type: ignore
-    new_model: str = ""
-    new_call_no: str = vodovod_pdf_reader(pdf_path=pdfpath)[2]  # type: ignore
-    new_period: tuple[date, date] = vodovod_pdf_reader(pdf_path=pdfpath)[3]  # type: ignore
-    # if business and business.id is not None:
-    #     create_bill(
-    #         bill_name=f"{business.type.value} - {date.today().strftime('%B')}",
-    #         bill_payed=True,
-    #         datepayed=date.today(),
-    #         bill_ammount=dict_amount,
-    #         businessid=business.id,
-    #         period=
-    #     )
 
     # df: pd.DataFrame = pd.DataFrame.from_dict(data=results, orient="columns")  # type: ignore
     # print(df.head())
